ebc5bc92fc46ae54f0b70337fc473d69e7354296
mariana-cunha <mari.marques26@gmail.com>
2023-08-10 16:42:50 -0300

    desafio queue

--- queue.js
+++ queue.js
@@ -3,8 +3,23 @@ class Queue {
   constructor(){
     this.elements = []
   }
+
+  size() {
+    return this.elements.length
+  }
+
+  add(item) {
+    return this.elements.push(item)
+  }
+
+  first(item) {
+    return item[0]
+  }
+
+  remove(item) {
+    return item.shift()
+  }
   
-  // Implementar aqui
 }
 
 module.exports = Queue
--- queue.test.js
+++ queue.test.js
@@ -8,18 +8,34 @@ describe('Queue', () => {
   })
 
   it('Deve ser capaz de calcular o tamanho da fila', () => {
-    // Seu código aqui
+    const queue = new Queue()
+
+    expect(queue.size()).toBe(0)
   })
 
   it('Deve ser capaz de adicionar um item na fila', () => {
-   // Seu código aqui
+    const queue = new Queue()
+
+    const names = ["ana", "beatriz"];
+    names.push("mariana")
+
+    expect(names).toContain("mariana")
+    expect(queue.add(names)).toBe(1)
   })
 
   it('Deve escolher o primeiro item da fila', () => {
-    // Seu código aqui
+    const queue = new Queue()
+
+    const arrayMockado = [6, 1, 2, 3]
+
+    expect(queue.first(arrayMockado)).toEqual(6)
   })
 
   it('Deve remover o primeiro item da fila', () => {
-    // Seu código aqui
+    const queue = new Queue()
+
+    const arrayMockado = [9, 1, 2, 3]
+
+    expect(queue.remove(arrayMockado)).toEqual(9)
   })
 })
